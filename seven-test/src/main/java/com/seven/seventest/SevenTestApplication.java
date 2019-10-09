eb3f1d592f53396601511d435bac8267e43a3129
seven <liangqihong007@163.com>
2019-10-09 09:17:26 +0800

    添加test

---
+++ seven-test/src/main/java/com/seven/seventest/SevenTestApplication.java
@@ -0,0 +1,13 @@
+package com.seven.seventest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevenTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SevenTestApplication.class, args);
+	}
+
+}
